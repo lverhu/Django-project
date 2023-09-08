6c5c63e44fe0c29d9e7d6fb2511ae1da46f8f8ce
lverhu <468031827@qq.com>
2023-09-08 23:24:00 +0800

    models

--- myproject/cases/models.py
+++ myproject/cases/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class CaseStore(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+
+    class Meta:
+        table_name = "tb_case_store"
+
+class CaseParam(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    casestore = models.ForeignKey(CaseStore, on_delete=models.CASCADE)
+    
+    class Meta:
+        table_name = "tb_case_param"
