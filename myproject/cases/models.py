7883f7c4e9ca9b4a7244a18bd21669d50fa54a52
lverhu <468031827@qq.com>
2023-09-08 23:52:04 +0800

    mysql

--- myproject/cases/models.py
+++ myproject/cases/models.py
@@ -5,13 +5,13 @@ class CaseStore(models.Model):
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=200)
 
-    class Meta:
-        table_name = "tb_case_store"
+    # class Meta:
+    #     table_name = "tb_case_store"
 
 class CaseParam(models.Model):
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=200)
     casestore = models.ForeignKey(CaseStore, on_delete=models.CASCADE)
-    
-    class Meta:
-        table_name = "tb_case_param"
+
+    # class Meta:
+    #     table_name = "tb_case_param"
